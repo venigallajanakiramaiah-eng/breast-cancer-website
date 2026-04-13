b4c8cbecd25af039b51e6af9d5e92385f9b5647a
Janaki ramaiah Venigalla <janakiramaiahvenigalla@Janakis-MacBook-Air.local>
2026-04-13 00:48:27 -0500

    Added PDF report download with charts and user data

--- app.py
+++ app.py
@@ -526,5 +526,5 @@ def download_report():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5003))
+    port = int(os.environ.get("PORT", 50033))
     app.run(host="0.0.0.0", port=port, debug=False)
